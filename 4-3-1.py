27b82fe7e9190dcacf40de7c1a1ca6ff5b7aee2a
Sangmin <lsm9275@gmail.com>
2020-12-29 23:04:05 +0900

    data type commit

---
+++ 4-3-1.py
@@ -0,0 +1,14 @@
+''' a = 'hello'
+print(type(a))
+print(a[0:3])
+
+for t in a:
+    print(type(t)) '''
+#리스트 자료형(순서O, 중복O, 수정o, 삭제o)
+#선언
+a = []
+b = list()
+c = [0,0,1,2]
+d = [0,1,'car','apple','apart']
+e = [0,1,['car','apple','apart']]
+
